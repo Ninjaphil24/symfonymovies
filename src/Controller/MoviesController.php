af2040b86506d3423dcfe65f70558a274f4f3aa0
unknown <philipmodino@gmail.com>
2022-10-11 18:57:30 +0200

    Add methods & defaults parameters

--- src/Controller/MoviesController.php
+++ src/Controller/MoviesController.php
@@ -8,12 +8,12 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class MoviesController extends AbstractController
 {
-    #[Route('/movies', name: 'app_movies')]
-    public function index(): JsonResponse
+    #[Route('/movies/{name}', name: 'movies', defaults:['name'=>null], methods:['GET','HEAD'])]
+    public function index($name): JsonResponse
     {
         return $this->json([
             'message' => 'Welcome to your new controller!',
-            'path' => 'src/Controller/MoviesController.php',
+            'path' => $name,
         ]);
     }
 }
