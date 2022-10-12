b719d5223fcc8255da3df4691a0e1069e1d8a36c
unknown <philipmodino@gmail.com>
2022-10-12 19:50:14 +0200

    Add routes to db

--- src/Controller/MoviesController.php
+++ src/Controller/MoviesController.php
@@ -33,105 +33,105 @@ class MoviesController extends AbstractController
         ]);
     }
 
-    #[Route('/movies/create', name: 'create_movie')]
-    public function create(Request $request): Response
-    {
-        $movie = new Movie();
-        $form = $this->createForm(MovieFormType::class, $movie);
+//     #[Route('/movies/create', name: 'create_movie')]
+//     public function create(Request $request): Response
+//     {
+//         $movie = new Movie();
+//         $form = $this->createForm(MovieFormType::class, $movie);
         
-        $form->handleRequest($request);
+//         $form->handleRequest($request);
 
-        if ($form->isSubmitted() && $form->isValid()) {
-            $newMovie = $form->getData();
-            $imagePath = $form->get('imagePath')->getData();
+//         if ($form->isSubmitted() && $form->isValid()) {
+//             $newMovie = $form->getData();
+//             $imagePath = $form->get('imagePath')->getData();
             
-            if ($imagePath) {
-                $newFileName = uniqid() . '.' . $imagePath->guessExtension();
+//             if ($imagePath) {
+//                 $newFileName = uniqid() . '.' . $imagePath->guessExtension();
 
-                try {
-                    $imagePath->move(
-                        $this->getParameter('kernel.project_dir') . '/public/uploads',
-                        $newFileName
-                    );
-                } catch (FileException $e) {
-                    return new Response($e->getMessage());
-                }
-                $newMovie->setUserId($this->getUser()->getId());
-                $newMovie->setImagePath('/uploads/' . $newFileName);
-            }
+//                 try {
+//                     $imagePath->move(
+//                         $this->getParameter('kernel.project_dir') . '/public/uploads',
+//                         $newFileName
+//                     );
+//                 } catch (FileException $e) {
+//                     return new Response($e->getMessage());
+//                 }
+//                 $newMovie->setUserId($this->getUser()->getId());
+//                 $newMovie->setImagePath('/uploads/' . $newFileName);
+//             }
 
-            $this->em->persist($newMovie);
-            $this->em->flush();
+//             $this->em->persist($newMovie);
+//             $this->em->flush();
 
-            return $this->redirectToRoute('movies');
-        }
+//             return $this->redirectToRoute('movies');
+//         }
 
-        return $this->render('movies/create.html.twig', [
-            'form' => $form->createView()
-        ]);
-    }
+//         return $this->render('movies/create.html.twig', [
+//             'form' => $form->createView()
+//         ]);
+//     }
 
-    #[Route('/movies/edit/{id}', name: 'edit_movie')]
-    public function edit($id, Request $request): Response 
-    {
-        $this->checkLoggedInUser($id);
-        $movie = $this->movieRepository->find($id);
+//     #[Route('/movies/edit/{id}', name: 'edit_movie')]
+//     public function edit($id, Request $request): Response 
+//     {
+//         $this->checkLoggedInUser($id);
+//         $movie = $this->movieRepository->find($id);
 
-        $form = $this->createForm(MovieFormType::class, $movie);
+//         $form = $this->createForm(MovieFormType::class, $movie);
 
-        $form->handleRequest($request);
-        $imagePath = $form->get('imagePath')->getData();
+//         $form->handleRequest($request);
+//         $imagePath = $form->get('imagePath')->getData();
 
-        if ($form->isSubmitted() && $form->isValid()) {
-            if ($imagePath) {
-                if ($movie->getImagePath() !== null) {
-                    if (file_exists(
-                        $this->getParameter('kernel.project_dir') . $movie->getImagePath()
-                        )) {
-                            $this->GetParameter('kernel.project_dir') . $movie->getImagePath();
-                    }
-                    $newFileName = uniqid() . '.' . $imagePath->guessExtension();
+//         if ($form->isSubmitted() && $form->isValid()) {
+//             if ($imagePath) {
+//                 if ($movie->getImagePath() !== null) {
+//                     if (file_exists(
+//                         $this->getParameter('kernel.project_dir') . $movie->getImagePath()
+//                         )) {
+//                             $this->GetParameter('kernel.project_dir') . $movie->getImagePath();
+//                     }
+//                     $newFileName = uniqid() . '.' . $imagePath->guessExtension();
 
-                    try {
-                        $imagePath->move(
-                            $this->getParameter('kernel.project_dir') . '/public/uploads',
-                            $newFileName
-                        );
-                    } catch (FileException $e) {
-                        return new Response($e->getMessage());
-                    }
+//                     try {
+//                         $imagePath->move(
+//                             $this->getParameter('kernel.project_dir') . '/public/uploads',
+//                             $newFileName
+//                         );
+//                     } catch (FileException $e) {
+//                         return new Response($e->getMessage());
+//                     }
 
-                    $movie->setImagePath('/uploads/' . $newFileName);
-                    $this->em->flush();
+//                     $movie->setImagePath('/uploads/' . $newFileName);
+//                     $this->em->flush();
 
-                    return $this->redirectToRoute('movies');
-                }
-            } else {
-                $movie->setTitle($form->get('title')->getData());
-                $movie->setReleaseYear($form->get('releaseYear')->getData());
-                $movie->setDescription($form->get('description')->getData());
+//                     return $this->redirectToRoute('movies');
+//                 }
+//             } else {
+//                 $movie->setTitle($form->get('title')->getData());
+//                 $movie->setReleaseYear($form->get('releaseYear')->getData());
+//                 $movie->setDescription($form->get('description')->getData());
 
-                $this->em->flush();
-                return $this->redirectToRoute('movies');
-            }
-        }
+//                 $this->em->flush();
+//                 return $this->redirectToRoute('movies');
+//             }
+//         }
 
-        return $this->render('movies/edit.html.twig', [
-            'movie' => $movie,
-            'form' => $form->createView()
-        ]);
-    }
+//         return $this->render('movies/edit.html.twig', [
+//             'movie' => $movie,
+//             'form' => $form->createView()
+//         ]);
+//     }
 
-    #[Route('/movies/delete/{id}', methods: ['GET', 'DELETE'], name: 'delete_movie')]
-    public function delete($id): Response
-    {
-        $this->checkLoggedInUser($id);
-        $movie = $this->movieRepository->find($id);
-        $this->em->remove($movie);
-        $this->em->flush();
+//     #[Route('/movies/delete/{id}', methods: ['GET', 'DELETE'], name: 'delete_movie')]
+//     public function delete($id): Response
+//     {
+//         $this->checkLoggedInUser($id);
+//         $movie = $this->movieRepository->find($id);
+//         $this->em->remove($movie);
+//         $this->em->flush();
 
-        return $this->redirectToRoute('movies');
-    }
+//         return $this->redirectToRoute('movies');
+//     }
 
     #[Route('/movies/{id}', methods: ['GET'], name: 'show_movie')]
     public function show($id): Response
